256cbf501a08d0aa37192fd0ef8cce8bd0790bda
Michael T <michae1T@live.ca>
2012-11-15 03:27:47 +0100

    initial config macbook air @ fc17

---
+++ src/bootstrap/common/postgres.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "configuring postgres"
+
+export PGHOME=/home/postgres
+export PGDATA=$PGHOME/data
+
+rm -rf $PGHOME
+mkdir $PGHOME
+
+chown postgres:postgres $PGHOME
+rm /etc/passwd.bak
+mv /etc/passwd /etc/passwd.bak
+sed s/\\/var\\/lib\\/pgsql/\\/home\\/postgres/ /etc/passwd.bak > /etc/passwd 
+
+su - postgres -c "PGDATA=$PGDATA postgresql-setup initdb" \
+ || su - postgres -c "PGDATA=$PGDATA initdb"
+
+./_fix_postgres_selinux.sh
+
+sleep 10
+
+systemctl enable postgresql.service
+systemctl start postgresql.service
+
+sleep 10
+
+su - postgres -c "createuser --no-superuser --no-createdb --no-createrole admin"
+su - postgres -c "createuser --no-superuser --no-createdb --no-createrole michael"
+
+rm -f $PGDATA/pg_hba.conf.bak
+cp -f $PGDATA/pg_hba.conf $PGDATA/pg_hba.conf.bak
+sed "s/ident\|peer/trust/g" \
+    $PGDATA/pg_hba.conf \
+    > pg_hba.conf
+mv -f pg_hba.conf $PGDATA/pg_hba.conf
+restorecon $PGDATA/pg_hba.conf
+
+systemctl restart postgresql.service
+
---
+++ src/bootstrap/gilt/hacks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# build submodule requires /usr/bin/ruby -> 1.8.7
+ln -s /opt/ruby-1.8.7/bin/ruby /usr/local/bin/ruby
+
