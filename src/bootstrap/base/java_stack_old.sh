b56f0159d5ee388ee8f7e191e0b0b89a3ca5dfcf
Michael T <michae1T@live.ca>
2012-11-19 18:34:39 -0500

    add mvn to old java deps

--- src/bootstrap/base/java_stack_old.sh
+++ src/bootstrap/base/java_stack_old.sh
@@ -2,7 +2,7 @@
 
 cd /opt/
 
-rm apache-ant* apache-ivy* > /dev/null 2>&1
+rm -rf apache-ant* apache-ivy* apache-maven* > /dev/null 2>&1
 
 wget http://archive.apache.org/dist/ant/binaries/apache-ant-1.8.2-bin.tar.gz
 tar -xvf apache-ant-1.8.2-bin.tar.gz
@@ -13,3 +13,8 @@ tar -xvf apache-ivy-2.3.0-rc2-bin.tar.gz
 rm apache-ivy-2.3.0-rc2-bin.tar.gz
 
 ln -s /opt/apache-ivy-2.3.0-rc2/ivy-2.3.0-rc2.jar /opt/apache-ant-1.8.2/lib/ivy.jar
+
+wget http://www.us.apache.org/dist/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz
+tar -xvf apache-maven-3.0.4-bin.tar.gz
+rm apache-maven-3.0.4-bin.tar.gz
+
