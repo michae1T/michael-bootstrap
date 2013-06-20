02aa76129f5111b79668dd17d1684ceff53c06f0
Michael T <michae1T@live.ca>
2013-06-20 17:41:36 -0400

    clean up env scripts add clock-in clock-out for sticky sessions

--- src/scripts/macbook-pro/slow-gfx.sh
+++ src/scripts/macbook-pro/slow-gfx.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo low > /sys/class/drm/card0/device/power_profile
+DEV0=/sys/class/drm/card0/device/power_profile
+DEV1=/sys/class/drm/card1/device/power_profile
+
+if [ -f "$DEV0" ] ;
+  then echo low > $DEV0
+fi;
+
+if [ -f "$DEV1" ] ;
+  then echo low > $DEV1
+fi;
+
