d455eb158e4c2942e5e1c236902f99adffa15a1d
Michael T <michae1T@live.ca>
2013-11-11 10:16:22 -0500

    gilt env to dny setup and freshen rails env config

--- src/bootstrap/gilt/rails.sh
+++ src/bootstrap/gilt/rails.sh
@@ -11,13 +11,20 @@ GEM_OPTS='--no-ri --no-rdoc'
 
 export PATH=$INSTALL_DIR/bin:$PATH
 
-### required system gems ###
+### setup rake ###
+
+# uninstall any newer versions of rake since they can interfere
+yes y | $GEM uninstall rake --version '>0.8.7' 2> /dev/null || echo 'no other versions of rake found'
 
 $GEM install rake --version=0.8.7 $GEM_OPTS
 
-### encyription stuff? ###
+### ruby aes ###
 
-source $SHARED/_ruby_aes.sh
+#wget http://aescrypt.sourceforge.net/aes-rb-0.1.0.tar.gz
+#tar -xvf aes-rb-0.1.0.tar.gz
+#cd aes-rb-0.1.0
+#/opt/ruby-1.8.7/bin/ruby extconf.rb && make && make install
+#cd .. && rm -rf aes-rb-0.1.0*
 
 ### libxml-ruby 1.1.3 needs to be patched for new versions of libxml2 ###
 
@@ -29,20 +36,20 @@ cd $RUBY_PROJECTS/libxml-ruby
 $RAKE gem
 $GEM install admin/pkg/libxml-ruby-1.1.3.gem $GEM_OPTS 
 
-
 ### extra stuff for capistrano deploys and general undocumented development :| ###
 
 $GEM install bundler08 thin rdoc capistrano rspec \
                              net-ssh-gateway net-ssh net-sftp net-scp \
-                             rails \
                              $GEM_OPTS
 
 
 $GEM install sinatra --version=1.2.6 $GEM_OPTS
 $GEM install assette --version=0.1.7 $GEM_OPTS
-
-
 $GEM install rake $GEM_OPTS
+
+write_env_script 'gilt' \
+  "export PGUSER=web\nexport PGDATABASE=gilt_us_development\n\nexport PATH=/opt/apache-ant-1.8.2/bin:/opt/apache-maven-3.0.4/bin:\$PATH\nexport ANT_HOME=/opt/apache-ant-1.8.2\n\nexport PATH=~/src/gilt/bin:~/src/gilt/tools/bin:~/src/gilt/play-2.0:\$PATH\n\nNO_SHELL=1 source set-ruby-old-env\nNO_SHELL=1 source set-java-6-env\n" \
+  "mvn -version | grep \"Apache Maven\"\nant -version\nsbt --version"
 
 cd $START_DIR
 
