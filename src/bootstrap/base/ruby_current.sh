3f8a3389585790c334838d9716aec19ad2d777fb
Michael T <michae1T@live.ca>
2013-11-11 08:26:03 -0500

    change ruby env scripts to dyn gen

--- src/bootstrap/base/ruby_current.sh
+++ src/bootstrap/base/ruby_current.sh
@@ -6,6 +6,7 @@ RUBY_INSTALL_DIR=/opt/ruby-1.9.3
 RUBY_TAG=origin/ruby_1_9_3
 GEM_TAG=origin/1.8
 RUBY_VERSION=1.9.3
+ENV_NAME=ruby-new
 
 source $SHARED/_setup_ruby.sh
 
--- src/bootstrap/base/ruby_old.sh
+++ src/bootstrap/base/ruby_old.sh
@@ -6,6 +6,7 @@ RUBY_INSTALL_DIR=/opt/ruby-1.8.7
 RUBY_TAG=origin/ruby_1_8_7
 GEM_TAG=v1.3.7
 RUBY_VERSION=1.8.7
+ENV_NAME=ruby-old
 
 export CC=gcc34
 export CXX=g++34
