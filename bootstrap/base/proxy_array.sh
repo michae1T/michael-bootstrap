9c383327b9f83e9c2cbb587b68c9d941ea9da69d
Michael T <michae1T@live.ca>
2015-02-18 16:42:22 -0500

    increase number of dynamic tunneling proxy nodes and organise them better

--- bootstrap/base/proxy_array.sh
+++ bootstrap/base/proxy_array.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
+# loadbalancer (haproxy) -> http proxies (polipo) -> socks tunnels (ssh)
+# user sets up array of dynamic socks tunnels, this provides infrastructure to feed
+# off them in a consistent manner
+#
+# map:
+# num - conn - socks - polipo - haprxy group - notes
+# 1   - gw1  - x     - x      -              - build only
+# 2   - gw1  - x     - x      -   x
+# 3   - gw1  - x     - x      -   x
+# 4   - gw2  - x     - x      -   x
+# 5   - gw2  - x     - x      -   x
+# 6   - gw2  - x     - x      -              - docker & other
+#
+# defaults: 
+#
+# dynamic socks tunnels  - 12345-12340
+# polipo http proxies    - 8123-8128
+# haproxy balanced proxy - 8120
+#
+
 source `dirname $0`/../_environment.sh
 
-if [ -n "BASE_PORT_IN" ] ;
+if [ -z "$BASE_PORT_IN" ] ;
   then BASE_PORT_IN=12344
 fi;
 
-if [ -n "BASE_PORT_OUT" ] ;
+if [ -z "$BASE_PORT_OUT" ] ;
   then BASE_PORT_OUT=8122
 fi;
 
-yum_safe install polipo
+if [ -z "$HAPROXY_CONFIG" ];
+  then HAPROXY_CONFIG=/etc/haproxy/haproxy.cfg
+fi;
+
+yum_safe install polipo haproxy
 
 systemctl disable polipo
 systemctl stop polipo
@@ -21,7 +45,7 @@ mkdir -p /etc/polipo/config.d
 
 update_config 'CONFIG_DIR' '/etc/polipo/config.d' '/etc/sysconfig/polipo' '='
 
-for i in 1 2 3 4 ; do
+for i in 1 2 3 4 5 6 ; do
 
   systemctl stop polipo${i}.service
 
@@ -67,4 +91,52 @@ EOF
   systemctl start polipo${i}.service
 
 done;
+
+# set up load balancer
+
+semanage port -a -t http_cache_port_t -p tcp 8120
+setsebool -P httpd_can_network_relay 1
+
+
+cat > $HAPROXY_CONFIG <<-EOF
+global
+    log         127.0.0.1 local2
+    chroot      /var/lib/haproxy
+    pidfile     /var/run/haproxy.pid
+    maxconn     8000
+    user        haproxy
+    group       haproxy
+    daemon
+    stats socket /var/lib/haproxy/stats
+defaults
+    mode                    tcp
+    log                     global
+    option                  httplog
+    option                  dontlognull
+    option http-server-close
+    option forwardfor       except 127.0.0.0/8
+    option                  redispatch
+    retries                 3
+    timeout http-request    10s
+    timeout queue           1m
+    timeout connect         10s
+    timeout client          1m
+    timeout server          1m
+    timeout http-keep-alive 10s
+    timeout check           10s
+    maxconn                 8000
+frontend  main *:8120
+    default_backend             app
+backend app
+    mode tcp
+    balance leastconn
+EOF
+
+for i in 1 2 3 4 ; do
+  OUT_PORT=$(($BASE_PORT_OUT + $i + 1))
+  echo "    server  app$i 127.0.0.1:$OUT_PORT check" >>  $HAPROXY_CONFIG
+done;
+
+systemctl enable haproxy.service
+systemctl restart haproxy.service
 
