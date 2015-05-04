30169c568957ed060ba553bd2b209a4db3362b3b
Michael T <michae1T@live.ca>
2015-05-04 16:55:53 -0400

    update android env

--- bootstrap/_environment.sh
+++ bootstrap/_environment.sh
@@ -120,7 +120,7 @@ write_env_script() {
   SCRIPT_PATH=`script_path $1-env`
   BODY=`echo -e $2 | sed -r 's/^\s+//'`
   SCPT=`echo -e $3 | sed 's/^/  /'`
-echo $BODY
+
   ensure_path_exists $SCRIPT_PATH
 
   cat > $SCRIPT_PATH <<- EOF
