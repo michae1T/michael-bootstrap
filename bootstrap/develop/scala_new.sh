922cd520ffa177c62af8b8bc5d90f40d70665ff2
Michael T <michae1T@live.ca>
2017-04-17 21:31:10 +0000

    update scala

--- bootstrap/develop/scala_new.sh
+++ bootstrap/develop/scala_new.sh
@@ -2,7 +2,7 @@
 
 source `dirname $0`/../_environment.sh
 
-SBT_VER=0.13.13
+SBT_VER=0.13.15
 SCALA_VER=2.12.1
 PLAY_VER=1.3.12
 
@@ -10,7 +10,7 @@ curl "https://dl.bintray.com/sbt/native-packages/sbt/$SBT_VER/sbt-$SBT_VER.zip" 
 unzip -o sbt-${SBT_VER}.zip
 rm sbt-${SBT_VER}.zip
 rm -rf /opt/sbt-$SBT_VER
-mv sbt-launcher-packaging-$SBT_VER /opt/sbt-$SBT_VER
+mv sbt /opt/sbt-$SBT_VER
 
 wget "http://www.scala-lang.org/files/archive/scala-$SCALA_VER.tgz"
 tar -xzf scala-$SCALA_VER.tgz
@@ -23,12 +23,12 @@ unzip -o typesafe-activator-$PLAY_VER-minimal.zip
 rm typesafe-activator-$PLAY_VER-minimal.zip
 rm -rf /opt/play-$PLAY_VER
 mv activator-$PLAY_VER-minimal /opt/play-$PLAY_VER
-chmod o+x /opt/play-$PLAY_VER/activator
+chmod o+x /opt/play-$PLAY_VER/bin/activator
 
 # predownload sbt runtimes
 su - $USER_OWNER -c "/opt/sbt-$SBT_VER/bin/sbt sbtVersion"
 
 write_env_script 'scala-new' \
-  "export PATH=/opt/scala-$SCALA_VER/bin:/opt/sbt-$SBT_VER/bin:/opt/play-$PLAY_VER:\$PATH" \
+  "export PATH=/opt/scala-$SCALA_VER/bin:/opt/sbt-$SBT_VER/bin:/opt/play-$PLAY_VER/bin:\$PATH" \
   "scala -version\nwhich sbt"
 
