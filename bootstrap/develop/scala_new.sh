cab6d09be824dfdcc4af39bb2c01beb7767570b1
Michael T <michae1T@live.ca>
2024-06-02 11:56:22 -0400

    scala update

--- bootstrap/develop/scala_new.sh
+++ bootstrap/develop/scala_new.sh
@@ -2,8 +2,8 @@
 
 source `dirname $0`/../_environment.sh
 
-SBT_VER=1.2.8
-SCALA_VER=2.12.8
+SBT_VER=1.10.0
+SCALA_VER=3.4.2
 
 curl "https://github.com/sbt/sbt/releases/download/v${SBT_VER}/sbt-${SBT_VER}.zip" -L > sbt-${SBT_VER}.zip
 unzip -o sbt-${SBT_VER}.zip
