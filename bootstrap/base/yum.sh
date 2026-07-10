0400feb0d371b4fc6f8e0ddba78299f17e8a4529
Michael T <michae1T@live.ca>
2026-07-10 11:33:08 -0400

    update batch

--- bootstrap/base/yum.sh
+++ bootstrap/base/yum.sh
@@ -7,7 +7,7 @@ yum_safe update
 yum_safe groupinstall 'Development Tools'
 
 yum_safe install git \
-               java-17-openjdk-headless java-17-openjdk-devel \
+               java-openjdk-headless java-openjdk-devel \
                perl perl-CPAN perl-Module-Load \
                wget curl lynx nmap \
                telnet nc bind-utils iputils iproute net-tools traceroute \
@@ -18,14 +18,15 @@ yum_safe install git \
                bash-completion \
                gdbm-devel openssl* openssh* \
                compat-readline* \
-               snakeyaml libyaml* \
+               libyaml* \
                cmake automake libtool rpm-build \
                patch readline* gettext* intltool \
                autoconf byacc bison \
                zlib zlib-devel libffi-devel \
                bzip2 sqlite sqlite-devel \
                libxslt* libxml2 libxml2-devel \
-               libpng-* libpng12 pangox-compat \
-               nodejs jq yajl \
-               grim swaylock wl-clipboard
+               libpng-* libpng12 \
+               jq yajl \
+               grim swaylock wl-clipboard \
+               byobu
 
--- bootstrap/develop/scala_new.sh
+++ bootstrap/develop/scala_new.sh
@@ -2,8 +2,8 @@
 
 source `dirname $0`/../_environment.sh
 
-SBT_VER=1.10.0
-SCALA_VER=3.4.2
+SBT_VER=1.10.5
+SCALA_VER=3.5.2
 
 curl "https://github.com/sbt/sbt/releases/download/v${SBT_VER}/sbt-${SBT_VER}.zip" -L > sbt-${SBT_VER}.zip
 unzip -o sbt-${SBT_VER}.zip
@@ -11,11 +11,11 @@ rm sbt-${SBT_VER}.zip
 rm -rf /opt/sbt-$SBT_VER
 mv sbt /opt/sbt-$SBT_VER
 
-wget "https://downloads.lightbend.com/scala/${SCALA_VER}/scala-${SCALA_VER}.tgz"
-tar -xzf scala-$SCALA_VER.tgz
-rm scala-$SCALA_VER.tgz
+wget "https://github.com/scala/scala3/releases/download/${SCALA_VER}/scala3-${SCALA_VER}.tar.gz"
+tar -xzf scala3-$SCALA_VER.tar.gz
+rm scala3-$SCALA_VER.tar.gz
 rm -rf /opt/scala-$SCALA_VER
-mv scala-$SCALA_VER /opt
+mv scala3-$SCALA_VER /opt/scala-$SCALA_VER
 
 # predownload sbt runtimes
 su - $USER_OWNER -c "/opt/sbt-$SBT_VER/bin/sbt sbtVersion"
